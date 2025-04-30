c63e4c9f5210e6883c77fec1064a7fb0d4f9bd8a
Fumbo <74.71.spartacus@gmail.com>
2025-04-30 16:04:50 +0500

    Написаны тесты на класс RpnEvaluator

--- Source/Shared/MathC.Math/Services/Evaluators/RpnEvaluator.cs
+++ Source/Shared/MathC.Math/Services/Evaluators/RpnEvaluator.cs
@@ -3,9 +3,11 @@ using System.Numerics;
 
 namespace MathC.Math.Services.Evaluators;
 
-public class RbnEvaluator<T> : IEvaluator<T, List<string>>
+public class RpnEvaluator<T>(OperationFactory<T> factory) : IEvaluator<T, List<string>>
     where T : INumber<T>
 {
+    private readonly OperationFactory<T> _factory = factory;
+
     public T Convert(List<string> convertedTokens)
     {
         throw new NotImplementedException();
