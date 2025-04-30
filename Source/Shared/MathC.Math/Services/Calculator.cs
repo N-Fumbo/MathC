4a04897b5875eb63c0a5b2fdd9228ba0f02291cb
Fumbo <74.71.spartacus@gmail.com>
2025-04-30 16:25:02 +0500

    Добавлен Calculator Написаны тесты на Calculator

---
+++ Source/Shared/MathC.Math/Services/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using MathC.Math.Interfaces;
+using System.Numerics;
+
+namespace MathC.Math.Services;
+
+public class Calculator<T> : ICalculator<T>
+    where T : INumber<T>
+{
+    public T Evaluate(string expression)
+    {
+        throw new NotImplementedException();
+    }
+}
