20869f272be3b2253b9f9845c172c115c87676ea
Fumbo <74.71.spartacus@gmail.com>
2025-04-30 14:25:17 +0500

    Написаны тесты на OperationFactory

---
+++ Source/Shared/MathC.Math/Services/OperationFactory.cs
@@ -0,0 +1,33 @@
+﻿using MathC.Math.Interfaces.Operation;
+using System.Numerics;
+
+namespace MathC.Math.Services;
+
+public class OperationFactory<T> : IOperationFactory<T>
+    where T : INumber<T>
+{
+    public OperationFactory()
+    {
+        
+    }
+
+    public IOperation<T> Get(string symbol)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsOperation(string token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Register(IOperation<T> operation)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryGet(string symbol, out IOperation<T> operation)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Tests/Shared/MathC.Math.Test/Services/OperationFactoryTesting.cs
@@ -0,0 +1,89 @@
+﻿using MathC.Math.Interfaces.Operation;
+using MathC.Math.Services;
+
+namespace MathC.Math.Test.Services;
+
+public class OperationFactoryTesting
+{
+    [Fact]
+    [Trait("Type", "Модульные тесты")]
+    public void Register_AddOperation_ShouldBeRecognized()
+    {
+        var factory = new OperationFactory<int>();
+
+        var add = new FakeAddOperation();
+
+        factory.Register(add);
+
+        Assert.True(factory.IsOperation("+"));
+    }
+
+    [Fact]
+    [Trait("Type", "Модульные тесты")]
+    public void Get_RegisteredOperation_ReturnsCorrectInstance()
+    {
+        var factory = new OperationFactory<int>();
+
+        var add = new FakeAddOperation();
+
+        factory.Register(add);
+
+        var op = factory.Get("+");
+
+        Assert.NotNull(op);
+
+        Assert.Equal(1, op.Precedence);
+
+        Assert.Equal(5, op.Apply(2, 3));
+    }
+
+    [Fact]
+    [Trait("Type", "Модульные тесты")]
+    public void TryGet_ReturnsTrue_AndCorrectOperation()
+    {
+        var factory = new OperationFactory<int>();
+
+        var add = new FakeAddOperation();
+
+        factory.Register(add);
+
+        var result = factory.TryGet("+", out var op);
+
+        Assert.True(result);
+
+        Assert.NotNull(op);
+
+        Assert.Equal(5, op.Apply(2, 3));
+    }
+
+    [Fact]
+    [Trait("Type", "Модульные тесты")]
+    public void TryGet_ReturnsFalse_WhenNotFound()
+    {
+        var factory = new OperationFactory<int>();
+
+        var result = factory.TryGet("*", out var op);
+
+        Assert.False(result);
+
+        Assert.Null(op);
+    }
+
+    [Fact]
+    [Trait("Type", "Модульные тесты")]
+    public void Get_ThrowsException_WhenNotRegistered()
+    {
+        var factory = new OperationFactory<int>();
+
+        Assert.Throws<KeyNotFoundException>(() => factory.Get("/"));
+    }
+
+    private class FakeAddOperation : IOperation<int>
+    {
+        public IReadOnlyList<string> Symbols => ["+"];
+
+        public int Precedence => 1;
+
+        public int Apply(int left, int right) => left + right;
+    }
+}
