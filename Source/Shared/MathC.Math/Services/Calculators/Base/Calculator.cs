dddbe0a56b6f3da1e360950eebff45424a0cb710
Fumbo <74.71.spartacus@gmail.com>
2025-04-30 16:56:17 +0500

    Реализован калькулятор

---
+++ Source/Shared/MathC.Math/Services/Calculators/Base/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using MathC.Math.Interfaces;
+using System.Numerics;
+
+namespace MathC.Math.Services.Calculators.Base;
+
+public class Calculator<TNumber, TConverter>(
+    IMathConverter<TConverter> converter,
+    IEvaluator<TNumber, TConverter> evaluator) : ICalculator<TNumber>
+
+    where TNumber : INumber<TNumber>
+    where TConverter : class
+{
+    private readonly IMathConverter<TConverter> _converter = converter;
+
+    private readonly IEvaluator<TNumber, TConverter> _evaluator = evaluator;
+
+    public virtual TNumber Evaluate(string expression)
+    {
+        var tokens = Tokenizer.Tokenize(expression);
+
+        var convetredTokens = _converter.Convert(tokens);
+
+        var result = _evaluator.Convert(convetredTokens);
+
+        return result;
+    }
+}
